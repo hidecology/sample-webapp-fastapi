cd2c697b66af284bd6a5924e43354f350a863c7f
hidecology <hidecology0302@gmail.com>
2023-11-04 11:35:47 +0900

    add sample of getting path parameters

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# パスパラメータを受け取る
+@app.get("/hello/{name}")
+async def hello(name):
+    return {"message": "Hello {0}".format(name)}
