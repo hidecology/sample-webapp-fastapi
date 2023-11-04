75c814478a8dbf89189766ac01b2c553d4fabb08
hidecology <hidecology0302@gmail.com>
2023-11-04 12:53:08 +0900

    add a custom http exception handler

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
 @app.get("/")
 async def root():
