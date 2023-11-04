ede1167b82656c5ec7844e0992d199411bc06bba
hidecology <hidecology0302@gmail.com>
2023-11-04 13:23:21 +0900

    implement code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -6,8 +6,12 @@ midTermResults = { 'english': 43, 'math': 27, 'japanese': 62 }
 
 @app.get("/midTermResults")
 async def getAllMidTermResults():
-    return {"message": "Hello World"}
+    return midTermResults
 
 @app.get("/midTermResults/{subject}")
 async def getAllMidTermResult(subject):
-    return {"message": "Hello World"}
+    try:
+        result = midTermResults[subject]
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Resource not found")
