33397ce0c632000c51575987d024f032f7a3280e
hidecology <hidecology0302@gmail.com>
2023-11-04 12:51:48 +0900

    return an error code

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def hello(name):
+    if len(name) < 3:
+        raise HTTPException(status_code=404, detail="Resource not found")
+    return {"message": "Hello {0}".format(name)}
